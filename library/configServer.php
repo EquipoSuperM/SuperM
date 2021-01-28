13f76d7790637c9a43453111ed802eb623b1b3da
NancyTendo <61803693+NancyTendo@users.noreply.github.com>
2021-01-28 02:28:48 -0600

    actualizacion1

--- library/configServer.php
+++ library/configServer.php
@@ -1,5 +1,9 @@
 <?php
-define("USER", "root");
-define("SERVER", "localhost");
-define("BD", "store");
-define("PASS", "");
+define("USER", "bde5716380efe0");
+define("SERVER", "eu-cdbr-west-03.cleardb.net");
+define("BD", "/heroku_cf3b0aae0e0ed30");
+define("PASS", "d7dfc26f");
+
+@
+
+/heroku_cf3b0aae0e0ed30?reconnect=true
